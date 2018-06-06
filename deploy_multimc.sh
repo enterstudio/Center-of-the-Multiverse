0447da1c65919f0cbe432a8faf76c954a4797e55
nikky <niklasbloedorn@gmx.de>
2018-06-06 18:58:46 +0200

    add multimc export

---
+++ deploy_multimc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
+
+echo "packing mmc instance"
+java -jar "$DIR/bootstrap-voodoo.jar" pack "$DIR/cotm.lock.json" mmc
+if [ ! $? -eq 0 ]; then
+    echo "Error in step: Pack MultiMC"
+    exit 1
+fi
+
+echo "please upload the mmc package to your web server"
+echo "you can provide the url to players"
+
