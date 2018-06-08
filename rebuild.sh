525c3a60eebc612122c0c6fa32304c6faacf70cc
AnsuzThurisaz <chris.thurisaz@gmail.com>
2018-06-08 14:35:10 -0400

    CotM 2.1.7.3 build/release

--- rebuild.sh
+++ rebuild.sh
@@ -3,16 +3,30 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-echo "flattening"
+echo "Flattening .yaml"
 java -jar "$DIR/bootstrap-voodoo.jar" flatten "$DIR/cotm.yaml" -o "$DIR/cotm.json"
 if [ ! $? -eq 0 ]; then
     echo "Error in step: Flatten"
     exit 1
 fi
 
-echo "building"
+echo "Building Modpack"
 java -jar "$DIR/bootstrap-voodoo.jar" build "$DIR/cotm.json" -o cotm.lock.json --force
 if [ ! $? -eq 0 ]; then
     echo "Error in step: Build"
+    exit 1
+fi
+
+echo "Packing for SK"
+java -jar "$DIR/bootstrap-voodoo.jar" pack "$DIR/cotm.lock.json" sk
+if [ ! $? -eq 0 ]; then
+    echo "Error in step: Pack SK"
+    exit 1
+fi
+
+echo "Packing Server"
+java -jar "$DIR/bootstrap-voodoo.jar" pack "$DIR/cotm.lock.json" server
+if [ ! $? -eq 0 ]; then
+    echo "Error in step: Pack Server"
     exit 1
 fi
