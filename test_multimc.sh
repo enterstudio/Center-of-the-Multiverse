720e8b5f5f0df87dad357eecc86a79e5935504c7
nikky <niklasbloedorn@gmx.de>
2018-06-12 16:13:47 +0200

    missed argument order in test script

--- test_multimc.sh
+++ test_multimc.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 echo "testing with mmc instance"
-java -jar "$DIR/bootstrap-voodoo.jar" test "$DIR/cotm.lock.json" mmc
+java -jar "$DIR/bootstrap-voodoo.jar" test mmc "$DIR/cotm.lock.json"
 if [ ! $? -eq 0 ]; then
     echo "Error in step: Test MultiMC"
     exit 1
